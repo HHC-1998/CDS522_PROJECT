d5e6703cad4d5b9184cb12823d317aaab8cc9bad
HHC-1998 <haochenhou@LN.hk>
2025-10-03 02:37:25 +0800

    完成对话框部分功能

--- Front_End/ai-front-end/src/ConversationRecord.jsx
+++ Front_End/ai-front-end/src/ConversationRecord.jsx
@@ -2,9 +2,24 @@ import React from "react";
 import "./ConversationRecord.css"
 
 export default class ConversationRecord extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            testList : ["test1", "test2", "test3", "test4", "test5", "test6", "test17", "test8", "test1", "test2", "test3", "test4", "test5", "test6", "test17"]
+        }
+    }
+
+
     render(){
         return(
-            <div className="Record"></div> 
+            <div className="Record">
+
+                {this.state.testList.map((test, index) => {
+                    return <div key={index}> {test} </div>
+                })}
+
+            </div>
         )
     }
 }
