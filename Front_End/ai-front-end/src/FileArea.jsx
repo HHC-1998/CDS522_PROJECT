7cde40a8007ac4b44eaa978fe0980152eb3fdb2f
HHC-1998 <haochenhou@LN.hk>
2025-10-03 02:08:34 +0800

    前端界面细节调整

--- Front_End/ai-front-end/src/FileArea.jsx
+++ Front_End/ai-front-end/src/FileArea.jsx
@@ -4,7 +4,27 @@ import "./FileArea.css"
 export default class FileArea extends React.Component{
     render(){
         return(
-        <div className="FileAreaDiv">  
+        <div className = "FileAreaDiv"> 
+
+            <h1>Complaint Information Form</h1>
+            <p>Date Received :</p> 
+            <p style={{width:"20%", display:"inline"}}>Source :</p>
+            <p style={{width:"60%", display:"inline"}}># </p>
+            <p>Type :</p>
+            <p>Name of Caller :</p>
+            <p>Contact No. :</p>
+            <p>Slope No. :</p>
+            <p>Location :</p>
+            <p>Nature of Request :</p>
+            <p>Subject Matter :</p>
+            <p>10-day Rule Due Date :</p>
+            <p>ICC Interim Reply Due Date :</p>
+            <p>ICC Final Reply Due Date :</p>
+            <p>Works Completion Due Date :</p>
+            <p>Fax to Contractor on :</p>
+            <p>Fax Page(s) 1 + </p>
+            <p>Case Details :</p>
+
         </div>
         )
     }
--- Front_End/ai-front-end/src/NavigationBar.jsx
+++ Front_End/ai-front-end/src/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./NavigationBar.css"
 
+// 导航栏 ： 目前由校标填充，将来可以做下拉列表，用户头像等
 export default class NavigationBar extends React.Component{
     render(){
         return(
