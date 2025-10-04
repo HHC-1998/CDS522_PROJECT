f923846dd07b8bfba6116b1620fd11fb742e7e8d
HHC-1998 <haochenhou@LN.hk>
2025-10-04 14:18:55 +0800

    更新

--- Back_End/Data.py
+++ Back_End/Data.py
@@ -1,29 +1,33 @@
 # 数据类：所有类型的扫描类都可以调用此类来生成对象（后可连接数据库）
 class Data:
 
-    def __init__(self, dateRecieve, source, hashtag, nameOfCaller, contactNo, slopeNo,
-                 TDRDD, ICCIRDD, ICCFRDD, faxToContractorOn, faxPages):
-        self.dateRecieve = dateRecieve
+    def __init__(self, dateReceived, source, hashtag, type, nameOfCaller, contactNo,
+                 slopeNo, location, natureOfRequest, subjectMatter, tenDayRuleDueDate,
+                 ICCInterimReplyDueDate, ICCFinalReplyDueDate, worksCompletionDueDate,
+                 faxToContractorOn, faxPages, caseDetails):
+        
+        self.dateReceived = dateReceived
         self.source = source
         self.hashtag = hashtag
-        self.type = "Null"
+        self.type = type
         self.nameOfCaller = nameOfCaller
         self.contactNo = contactNo
         self.slopeNo = slopeNo
-        self.location = "Null"
-        self.natureOfRequest = "Null"
-        self.subjectMatter = "Null"
-        self.TDRDD = TDRDD
-        self.ICCIRDD = ICCIRDD
-        self.ICCFRDD = ICCFRDD
-        self.worksCompletionDueDate = "Null"
+        self.location = location
+        self.natureOfRequest = natureOfRequest
+        self.subjectMatter = subjectMatter
+        self.tenDayRuleDueDate = tenDayRuleDueDate
+        self.ICCInterimReplyDueDate = ICCInterimReplyDueDate
+        self.ICCFinalReplyDueDate = ICCFinalReplyDueDate
+        self.worksCompletionDueDate = worksCompletionDueDate
         self.faxToContractorOn = faxToContractorOn
         self.faxPages = faxPages
-        self.caseDetails = "Null"
+        self.caseDetails = caseDetails
 
+    # 重写toString方法
     def __str__(self):
-        return (f"a = {self.dateRecieve}\nb = {self.source}\nc = {self.hashtag}\n"
-                f"d = {self.type}\ne = {self.nameOfCaller}\nf = {self.contactNo}\ng = {self.slopeNo}\n"
-                f"h = {self.location}\ni = {self.natureOfRequest}\nj = {self.subjectMatter}\nk = {self.TDRDD}\n"
-                f"l = {self.ICCIRDD}\nm = {self.ICCFRDD}\nn = {self.worksCompletionDueDate}\n"
+        return (f"a = {self.dateReceived}\nb = {self.source}\nc = {self.hashtag}\nd = {self.type}\n"
+                f"e = {self.nameOfCaller}\nf = {self.contactNo}\ng = {self.slopeNo}\n"
+                f"h = {self.location}\ni = {self.natureOfRequest}\nj = {self.subjectMatter}\nk = {self.tenDayRuleDueDate}\n"
+                f"l = {self.ICCInterimReplyDueDate}\nm = {self.ICCFinalReplyDueDate}\nn = {self.worksCompletionDueDate}\n"
                 f"o = {self.faxToContractorOn}\np = {self.faxPages}\nq = {self.caseDetails}")
--- Back_End/ScanTxt.py
+++ Back_End/ScanTxt.py
@@ -20,22 +20,27 @@ class ScanTxt:
                 return "Null"
 
         # 需要查找的值
-        a, c, e, f1, f2, g, k, l, m = [""] * 9
+        a, b, c, d, e, f1, f2, g, h, i, j, k, l, m, n, o, p, q = [""] * 18
+
         # 逐行遍历该字符串
         for i in txtContent.split('\n'):
-            if i.startswith("Case Creation Date") : a = processStr(i)                       # a
-            if i.startswith("1823 CASE") : c = processStr(i)                                # c
-            if i.startswith("Last Name") : e = processStr(i)                                # e
-            if i.startswith("Mobile") : f1 = processStr(i)                                  # f1
-            if i.startswith("Email Address") : f2 = processStr(i)                           # f2
-            if i.startswith("投訴人") : g = i                                                # g
-            if i.startswith("Re-assignment") : k = processStr(i)                            # k
-            if i.startswith("Interim Reply") : l = processStr(i)                            # l
-            if i.startswith("Final Reply") : m = processStr(i)                              # m
-        # 特定值或未找到值
-        b, o, p = "ICC", a, "Con not found"
+            if i.startswith("Case Creation Date") : a = processStr(i)                 # a
+            if i.startswith("1823 CASE") : c = processStr(i)                          # c
+            if i.startswith("Last Name") : e = processStr(i)                          # e
+            if i.startswith("Mobile") : f1 = processStr(i)                            # f1
+            if i.startswith("Email Address") : f2 = processStr(i)                     # f2
+            if i.startswith("Re-assignment") : k = processStr(i)                      # k
+            if i.startswith("Interim Reply") : l = processStr(i)                      # l
+            if i.startswith("Final Reply") : m = processStr(i)                        # m
+
+        # 定义特定的值
+        f = f1 + "/" + f2
+        d, h, i, j, q = "Null"
+        b = "ICC"
+        o = a
+        p = "Connot Found"
 
         # 返回一个数据对象 
-        extractInfo = Data(a, b, c, e, f1+"/"+f2, g, k, l, m, o, p)
+        extractInfo = Data(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q)
         return extractInfo
 
